fc358c1c2244b40a016a33c112662adfc46d08a0
Michael Clark <michaeljclark@mac.com>
2025-07-20 11:12:36 +1200

    add test program to output 7-bit binary table for fp7

---
+++ test/t2.c
@@ -0,0 +1,72 @@
+#undef NDEBUG
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <wchar.h>
+
+#include "vf128.h"
+
+static int format_byte(char *buf, size_t buflen, uint8_t c)
+{
+    static const char arr[4][4] = { "▄", "▟", "▙", "█" };
+
+    int n = 0;
+    for (intptr_t i = 6; i >= 0; i-=2) {
+        if (n < buflen) {
+            int ret = snprintf(buf+n, buflen-n, "%s", arr[(c>>i) & 3]);
+            n += ret > 0 ? ret : 0;
+        }
+    }
+    return n;
+}
+
+int format_vf128(char *buf, size_t buflen, uint8_t v)
+{
+    float f;
+    char s[16] = { 0 };
+    int n = 0;
+    vf_buf *vfbuf = vf_buf_new(1);
+    vf_buf_write_bytes(vfbuf, (const char*)&v, 1);
+    vf_buf_reset(vfbuf);
+    vf_f32_read(vfbuf, &f);
+    format_byte(s, sizeof(s), v);
+    if (isnan(f)) {
+        union { float f; unsigned x; } u = { f };
+        char x[32];
+        snprintf(x, sizeof(x), "%snan(0x%x)", (u.x >> 31) ? "-" : "", v & 0xf);
+        n = snprintf(buf, buflen, "%-3d %s %9s", v, s, x);
+    }
+    else {
+        n = snprintf(buf, buflen, "%-3d %s %9.4f", v, s, f);
+    }
+    vf_buf_destroy(vfbuf);
+    return n;
+}
+
+int string_width(const char *buf)
+{
+    mbstate_t t = { 0 };
+    return mbsrtowcs (NULL, &buf, strlen(buf), &t);
+}
+
+void print_table(int ncols)
+{
+    char buf[64];
+    int nrows = 128/ncols;
+    for (uint8_t c = 0; c < ncols; c++) {
+        for (uint8_t r = 0; r < nrows; r++) {
+            uint8_t v = c*nrows + r;
+            int n = format_vf128(buf, sizeof(buf), v);
+	    int w = string_width(buf);
+            printf("%s%s%s", buf, "    ",
+                   (v % ncols == ncols-1) ? "\n" : "");
+        }
+    }
+}
+
+int main()
+{
+    print_table(4);
+    return 0;
+}
